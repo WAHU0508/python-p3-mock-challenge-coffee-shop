5fae7046b567fb2f97109b11bb95f6c44fae2851
WAHU0508 <sharongikenye@gmail.com>
2024-09-05 15:14:17 +0300

    Initialize Coffee and Customer classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,18 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if hasattr(self, '_name'):
+            raise AttributeError("Cannot change the coffee name once it has been set.")
+        if not isinstance(name, str):
+            raise TypeError("Coffee name must be of string type.")
+        if len(name) < 3:
+            raise ValueError("Customer name should not be less than 3 characters.")
+        self._name = name
     def orders(self):
         pass
     
@@ -17,7 +28,16 @@ class Coffee:
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Customer name must be of string type.")
+        if not 1<= len(name) <= 15:
+            raise ValueError("Customer name characters not between 1 and 15.")
+        self._name = name
     def orders(self):
         pass
     
@@ -28,7 +48,9 @@ class Customer:
         pass
     
 class Order:
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+
